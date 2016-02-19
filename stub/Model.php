21de6ba6cf8148104ad03b44be3a7a58e8376719
Jason Daly <jason@deefour.me>
2016-02-19 15:52:01 -0500

    bugfix tests

--- stub/Model.php
+++ stub/Model.php
@@ -5,9 +5,10 @@ namespace Deefour\Presenter\Stubs;
 use Deefour\Presenter\Contracts\Presentable;
 use Deefour\Presenter\ProducesPresenters;
 use Deefour\Producer\ResolvesProducibles;
+use Deefour\Producer\ProducesClasses;
 use Illuminate\Support\Fluent;
 
 abstract class Model extends Fluent implements Presentable
 {
-    use ProducesPresenters, ResolvesProducibles;
+    use ProducesPresenters, ResolvesProducibles, ProducesClasses;
 }
--- stub/Tag.php
+++ stub/Tag.php
@@ -4,8 +4,9 @@ namespace Deefour\Presenter\Stubs;
 
 use Deefour\Presenter\ProducesPresenters;
 use Deefour\Producer\Contracts\Producer;
+use Deefour\Producer\ProducesClasses;
 
 class Tag implements Producer
 {
-    use ProducesPresenters;
+    use ProducesPresenters, ProducesClasses;
 }
